9e4db3caa6f2086d1330c5e590fe14516ee6a3ff
AlexKhsn <alexkhomishyn@gmail.com>
2025-11-18 16:20:07 +0100

    Added TodoRepository

---
+++ src/main/kotlin/com/example/todoapi/repository/TodoRepository.kt
@@ -0,0 +1,10 @@
+package com.example.todoapi.repository
+
+import com.example.todoapi.entity.Todo
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TodoRepository : JpaRepository<Todo, Long> {
+    fun findByCompleted(completed: Boolean) : List<Todo>
+}
